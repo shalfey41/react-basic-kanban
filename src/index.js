12ab021af7e036cae728f70284d4dc75207dbdac
Dmitrii Bezuglyi <d.bezugly@corp.vk.com>
2020-02-27 03:49:50 +0300

    Инициализировали проект и убрали лишнее

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import connect from '@vkontakte/vk-connect';
-import App from './App';
-// import registerServiceWorker from './sw';
+// import connect from '@vkontakte/vk-connect';
+
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+import App from './components/App';
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyCOQlL4ImWDyGyG3yq1QzCpoEL7Kcmh46w",
+  authDomain: "kanban-efd5b.firebaseapp.com",
+  databaseURL: "https://kanban-efd5b.firebaseio.com",
+  projectId: "kanban-efd5b",
+  storageBucket: "kanban-efd5b.appspot.com",
+  messagingSenderId: "248459954323",
+  appId: "1:248459954323:web:772d44761733f23afa2566",
+  measurementId: "G-6GZ5H2FY0R"
+});
+firebase.analytics();
 
 // Init VK  Mini App
-connect.send('VKWebAppInit');
-
-// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
-// расскомментируйте строку с registerServiceWorker();
-// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
-// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
-// registerServiceWorker();
+// connect.send('VKWebAppInit');
 
 ReactDOM.render(<App />, document.getElementById('root'));
